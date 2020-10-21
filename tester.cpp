a487c85268d8b2c03aad9008d68385917c8fe9f7
ajstef20 <angela.stefanovska@richmond.edu>
2020-10-21 10:11:40 -0400

    changes to Rectangle testing

--- tester.cpp
+++ tester.cpp
@@ -5,6 +5,11 @@
 #include "Rectangle.h"
 
 using namespace std;
+/*
+void printArea(Shape &fig) {
+	cout << fig.getArea() << endl;
+}
+*/
 
 int main() {
 
@@ -31,20 +36,34 @@ int main() {
 	cout << "...Expected[circleTwo; radius = 0.02, radius = .01256]" << endl;
 
 	cout << "third circle (constructor)" << endl;
-        c3.print();
+	c3.print();
 	cout << "...Expectedd[circleThree; radius = 240.2,  radius = 181165.5656]"
 		 << endl;
 
 //test Triangle
 
 //test Rectangle
-	cout << "======== testing circle  =========="
-	cout << endl;
-	Rectangle r1;
-	r1.print();
+//test Rectangle
+        cout << "======== testing rectangle  ==========" << endl;
+        cout << endl;
+        cout << "Testing Rectangle Default Constructor: " << endl;
+        Rectangle r1;
+        r1.print();
+        cout << "[Shape: Rectangle]" << endl;
+        cout << "[Width: 0]" << endl;
+        cout << "[Height: 0]" << endl;
+        cout << "[Area: 0]" << endl;
 
-	cout << endl;
-	Rectangle r2("Rectangle1", 5.0, 2.0);
-	r2.print();
+        cout << endl;
+        cout << "Testing Constructor: " << endl;
+        Rectangle r2("Rectangle2", 5.0, 2.0);
+        r2.print();
+        cout << "[Shape: Rectangle2]" << endl;
+        cout << "[Width: 5]" << endl;
+        cout << "[Height: 2]" << endl;
+        cout << "[Area: 10]" << endl;
 
+	// printArea(r1);
 }
+
+
