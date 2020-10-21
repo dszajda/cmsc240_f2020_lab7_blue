1ed1d728552114d8708f77e027ddc77c9b3014a4
ca5pd <ca5pd@mathcs01.richmond.edu>
2020-10-20 22:20:50 -0400

    CHANGES BEING MADE

--- tester.cpp
+++ tester.cpp
@@ -8,4 +8,13 @@ int main() {
 
     figure.print();
     cout << figure.getArea() << endl;
+
+    //Triangle t;
+    Triangle t = new Triangle("Triangle 1", 4, 4 );
+    t.print();
+    cout << "Area: " << t.getArea() << endl;
+
+    Triangle t2 = new Triangle("Triangle 2", 0, 0);
+    t2.print();
+    cout << "Area: " << t2.getArea() << endl;
 }
